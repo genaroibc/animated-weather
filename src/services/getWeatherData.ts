148886d2117b0e230e11d2a883ca74d0ed693a14
genaroibc <bonavitaismael@gmail.com>
2023-01-24 13:24:47 -0300

    [DEV]: add getWeatherData service

---
+++ src/services/getWeatherData.ts
@@ -0,0 +1,17 @@
+import { Weather } from '../types';
+
+const { API_URL, API_KEY } = process.env;
+const ERROR_MESSAGE = 'env variable is not defined';
+
+if (!API_URL) throw new Error(`"API_URL" ${ERROR_MESSAGE}`);
+if (!API_KEY) throw new Error(`"API_KEY" ${ERROR_MESSAGE}`);
+
+type Params = {
+	location: string;
+};
+
+export function getWeatherData({ location }: Params): Promise<Weather> {
+	const URL = `${API_URL}?appid=${API_KEY}&q=${location}&units=metric`;
+
+	return fetch(URL).then((res) => res.json());
+}
