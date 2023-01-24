2846cf9065d1d81ec535751bd7a3f72e8591a316
genaroibc <bonavitaismael@gmail.com>
2023-01-24 17:23:46 -0300

    [FEAT]: add main composition

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,18 +1,49 @@
-import {Composition} from 'remotion';
-import {MyComposition} from './Composition';
+import { useCallback, useEffect, useState } from 'react';
+import { Composition, continueRender, delayRender } from 'remotion';
+import { MainComposition } from './compositions/MainComposition';
+import { DURATION, FPS } from './constants';
+import { getWeatherData } from './services/getWeatherData';
 import './style.css';
+import { Weather } from './types/api-reponse';
 
 export const RemotionRoot: React.FC = () => {
+	const [weatherData, setWeatherData] = useState<Weather | null>(null);
+	const [handle] = useState(() => delayRender());
+
+	const handleGetWeatherData = useCallback(async () => {
+		const persistedWeatherData = JSON.parse(
+			localStorage.getItem('weatherData') ?? 'null'
+		);
+
+		if (!persistedWeatherData) {
+			const weatherData = await getWeatherData({ location: 'london' });
+			localStorage.setItem('weatherData', JSON.stringify(weatherData));
+			setWeatherData(weatherData);
+
+			return;
+		}
+
+		setWeatherData(persistedWeatherData);
+		continueRender(handle);
+	}, [handle]);
+
+	useEffect(() => {
+		handleGetWeatherData();
+	}, [handleGetWeatherData]);
+
 	return (
 		<>
-			<Composition
-				id="MyComp"
-				component={MyComposition}
-				durationInFrames={240}
-				fps={30}
-				width={1280}
-				height={720}
-			/>
+			{weatherData && (
+				<Composition
+					id="MyComposition"
+					component={MainComposition}
+					defaultProps={weatherData}
+					fps={FPS}
+					durationInFrames={DURATION}
+					width={500}
+					height={500}
+				/>
+			)}
 		</>
 	);
 };
