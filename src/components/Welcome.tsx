7b41e0668a6d024ab3078fd32ba4d39e43e2f629
genaroibc <bonavitaismael@gmail.com>
2023-01-28 13:08:24 -0300

    [FEAT]: add Welcome component animation and weather emoji
     #	src/utils/

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,28 +1,26 @@
+import { spring, useCurrentFrame, useVideoConfig } from 'remotion';
 import { Weather } from '../types/api-reponse';
 import { CompositionLayout } from './shared/CompositionLayout';
+
 type Props = {
 	city: Weather['name'];
 };
 
 export function Title({ city }: Props) {
-	// Const scale = spring({ fps, frame });
+	const frame = useCurrentFrame();
+	const { fps } = useVideoConfig();
+	const scale = spring({ fps, frame });
 
 	return (
 		<CompositionLayout>
-			{/* <span
-				className="text-6xl animate-bounce"
-				style={{ animationDirection: 'alternate' }}
-			>
-      👋
-			</span> */}
-			{/* <h4
-				className="text-5xl opacity-1"
+			<span className="text-6xl animate-bounce">🌤️</span>
+			<p className="text-2xl m-0">This is the climate in</p>
+			<h1
+				className="text-white text-6xl text-center w-full m-0"
 				style={{ transform: `scale(${scale})` }}
 			>
-      ☀️
-			</h4> */}
-			<p className="text-2xl m-0">This is the climate in</p>
-			<h1 className="text-white text-6xl text-center w-full m-0 ">{city}</h1>
+				{city}
+			</h1>
 		</CompositionLayout>
 	);
 }
