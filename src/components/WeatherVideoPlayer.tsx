8e0608b2861c7588a28e2e78ea044d3411139e1c
genaroibc <bonavitaismael@gmail.com>
2023-01-31 19:17:26 -0300

    [REFACTOR]: separate logic into components

--- src/components/WeatherVideoPlayer.tsx
+++ src/components/WeatherVideoPlayer.tsx
@@ -9,50 +9,64 @@ import { Player } from '@remotion/player';
 
 export function WeatherVideoPlayer() {
 	return (
-		<Player
-			controls
-			component={MainComposition}
-			compositionHeight={COMPOSITION_HEIGHT}
-			compositionWidth={COMPOSITION_WIDTH}
-			durationInFrames={DURATION_IN_FRAMES}
-			fps={FPS}
-			inputProps={{
-				coord: { lon: -0.1257, lat: 51.5085 },
-				weather: [
-					{
-						id: 804,
-						main: 'Clouds',
-						description: 'overcast clouds',
-						icon: '04d',
-					},
-				],
-				base: 'stations',
-				main: {
-					temp: 4.12,
-					feels_like: 1.8,
-					temp_min: 2.86,
-					temp_max: 5.56,
-					pressure: 1037,
-					humidity: 80,
-				},
-				visibility: 10_000, // in metters. Max is 10_000 metters
-				wind: { speed: 2.57 /* metters/second */, deg: 60 }, // wind direction in degrees
-				clouds: {
-					all: 100 /* percentage of clouds. 100% is so cloudly (implement a message for this) */,
-				},
-				dt: 1674576100,
-				sys: {
-					type: 2,
-					id: 2075535,
-					country: 'GB',
-					sunrise: 1674546627,
-					sunset: 1674578053,
-				},
-				timezone: 0, // utc time zone
-				id: 2643743,
-				name: 'London',
-				cod: 200,
+		<div
+			style={{
+				textAlign: 'center',
+				backgroundColor: 'var(--terciary-color)',
+				maxWidth: 'max-content',
+				margin: 'auto',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				padding: '1rem',
+				borderRadius: '5px',
 			}}
-		/>
+		>
+			<Player
+				controls
+				component={MainComposition}
+				compositionHeight={COMPOSITION_HEIGHT}
+				compositionWidth={COMPOSITION_WIDTH}
+				durationInFrames={DURATION_IN_FRAMES}
+				fps={FPS}
+				inputProps={{
+					coord: { lon: -0.1257, lat: 51.5085 },
+					weather: [
+						{
+							id: 804,
+							main: 'Clouds',
+							description: 'overcast clouds',
+							icon: '04d',
+						},
+					],
+					base: 'stations',
+					main: {
+						temp: 4.12,
+						feels_like: 1.8,
+						temp_min: 2.86,
+						temp_max: 5.56,
+						pressure: 1037,
+						humidity: 80,
+					},
+					visibility: 10_000, // in metters. Max is 10_000 metters
+					wind: { speed: 2.57 /* metters/second */, deg: 60 }, // wind direction in degrees
+					clouds: {
+						all: 100 /* percentage of clouds. 100% is so cloudly (implement a message for this) */,
+					},
+					dt: 1674576100,
+					sys: {
+						type: 2,
+						id: 2075535,
+						country: 'GB',
+						sunrise: 1674546627,
+						sunset: 1674578053,
+					},
+					timezone: 0, // utc time zone
+					id: 2643743,
+					name: 'London',
+					cod: 200,
+				}}
+			/>
+		</div>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { WeatherVideoPlayer } from '@/components/WeatherVideoPlayer';
+import { LocationForm } from '@/components/LocationForm';
+import { Hero } from '@/components/Hero';
 
 const LOCATION_INPUT_NAME = 'user-location';
 
@@ -47,90 +49,16 @@ export default function Home() {
 				gap: '1rem',
 			}}
 		>
-			<header>
-				<h1
-					style={{ fontSize: '4rem', textAlign: 'center', margin: '3rem auto' }}
-				>
-					Animated Weather
-				</h1>
-				<h2 style={{ fontSize: '2rem', textAlign: 'center' }}>
-					Create an animated video of the weather in your zone
-				</h2>
-			</header>
+			<Hero />
 
-			<section
-				style={{
-					display: 'flex',
-					gap: '1rem',
-					justifyContent: 'center',
-					alignItems: 'center',
-					backgroundColor: 'var(--secondary-color)',
-					borderRadius: '5px',
-
-					padding: '2rem',
-					margin: '3rem auto',
-				}}
-			>
-				<form
-					style={{
-						display: 'flex',
-						gap: '1rem',
-						justifyContent: 'center',
-						alignItems: 'center',
-						backgroundColor: 'var(--terciary-color)',
-						borderRadius: '5px',
-						padding: '1rem',
-					}}
-					onSubmit={handleSubmit}
-				>
-					<label style={{ fontSize: '1.2rem' }} htmlFor={LOCATION_INPUT_NAME}>
-						Your location
-					</label>
-					<input
-						required
-						type="text"
-						name={LOCATION_INPUT_NAME}
-						id={LOCATION_INPUT_NAME}
-						placeholder="London"
-					/>
-
-					<button type="submit">Submit</button>
-				</form>
-
-				<h3 id="location-form-divider">or</h3>
-
-				<div
-					style={{
-						backgroundColor: 'var(--terciary-color)',
-						borderRadius: '5px',
-						padding: '1rem',
-					}}
-				>
-					<button type="button" onClick={handleDetectUserLocation}>
-						Detect my location 🔎
-					</button>
-				</div>
-			</section>
+			<LocationForm
+				handleDetectUserLocation={handleDetectUserLocation}
+				handleSubmit={handleSubmit}
+				inputName={LOCATION_INPUT_NAME}
+			/>
 
 			{userLocation ? (
-				<>
-					<h1>{JSON.stringify(userLocation)}</h1>
-					<div
-						style={{
-							textAlign: 'center',
-							backgroundColor: 'var(--terciary-color)',
-							maxWidth: 'max-content',
-							margin: 'auto',
-							display: 'flex',
-							justifyContent: 'center',
-							alignItems: 'center',
-							padding: '1rem',
-							borderRadius: '5px',
-						}}
-					>
-						<WeatherVideoPlayer />
-					</div>
-				</>
+				<WeatherVideoPlayer />
 			) : (
 				<h3>Enter a location to render your video...</h3>
 			)}
