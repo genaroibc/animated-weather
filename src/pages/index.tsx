0ffff51d20441f9e0e3bd3c31acab5e8b754a3a6
genaroibc <bonavitaismael@gmail.com>
2023-01-31 19:05:19 -0300

    [FEAT]: get user location data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,42 @@
+import { useState } from 'react';
 import { WeatherVideoPlayer } from '@/components/WeatherVideoPlayer';
 
+const LOCATION_INPUT_NAME = 'user-location';
+
+type LocationState = string | { longitude: number; latitude: number };
+
 export default function Home() {
+	const [userLocation, setUserLocation] = useState<LocationState>();
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		const form = e.target as HTMLFormElement;
+
+		const location = form[LOCATION_INPUT_NAME]?.value;
+		const userLocation = location?.trim();
+
+		if (userLocation) {
+			setUserLocation(userLocation);
+		}
+	};
+
+	const handleDetectUserLocation = () => {
+		const onSuccess: PositionCallback = (res) => {
+			console.log({ res });
+			const { latitude, longitude } = res.coords;
+
+			setUserLocation({ latitude, longitude });
+		};
+
+		const onError: PositionErrorCallback = () => {
+			setUserLocation('There was an error accessing to your location.');
+			// -> TODO: setError()
+		};
+
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	};
+
 	return (
 		<main
 			style={{
@@ -45,15 +81,16 @@ export default function Home() {
 						borderRadius: '5px',
 						padding: '1rem',
 					}}
+					onSubmit={handleSubmit}
 				>
-					<label style={{ fontSize: '1.2rem' }} htmlFor="user-location">
+					<label style={{ fontSize: '1.2rem' }} htmlFor={LOCATION_INPUT_NAME}>
 						Your location
 					</label>
 					<input
 						required
 						type="text"
-						name="user-location"
-						id="user-location"
+						name={LOCATION_INPUT_NAME}
+						id={LOCATION_INPUT_NAME}
 						placeholder="London"
 					/>
 
@@ -69,13 +106,34 @@ export default function Home() {
 						padding: '1rem',
 					}}
 				>
-					<button type="button">Detect my location 🔎</button>
+					<button type="button" onClick={handleDetectUserLocation}>
+						Detect my location 🔎
+					</button>
 				</div>
 			</section>
 
-			<div style={{ textAlign: 'center' }}>
-				<WeatherVideoPlayer />
-			</div>
+			{userLocation ? (
+				<>
+					<h1>{JSON.stringify(userLocation)}</h1>
+					<div
+						style={{
+							textAlign: 'center',
+							backgroundColor: 'var(--terciary-color)',
+							maxWidth: 'max-content',
+							margin: 'auto',
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+							padding: '1rem',
+							borderRadius: '5px',
+						}}
+					>
+						<WeatherVideoPlayer />
+					</div>
+				</>
+			) : (
+				<h3>Enter a location to render your video...</h3>
+			)}
 		</main>
 	);
 }
