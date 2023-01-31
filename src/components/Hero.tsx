404d7727a7f5b227c2b590e831bcb9cc356bb521
genaroibc <bonavitaismael@gmail.com>
2023-01-31 19:16:04 -0300

    [DEV]: add Hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+export function Hero() {
+	return (
+		<header>
+			<h1
+				style={{ fontSize: '4rem', textAlign: 'center', margin: '3rem auto' }}
+			>
+				Animated Weather
+			</h1>
+			<h2 style={{ fontSize: '2rem', textAlign: 'center' }}>
+				Create an animated video of the weather in your zone
+			</h2>
+		</header>
+	);
+}
