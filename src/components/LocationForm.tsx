41da82a4a406ddce38573c0d30b4fdda961e14e0
genaroibc <bonavitaismael@gmail.com>
2023-02-08 16:15:38 -0300

    [FEAT]: replace inlice CSS with tailwind and update props

--- src/components/LocationForm.tsx
+++ src/components/LocationForm.tsx
@@ -1,67 +1,26 @@
 type Props = {
 	handleSubmit: (e: React.FormEvent) => void;
-	handleDetectUserLocation: () => void;
 	inputName: string;
 };
 
-export function LocationForm({
-	handleSubmit,
-	handleDetectUserLocation,
-	inputName,
-}: Props) {
+export function LocationForm({ handleSubmit, inputName }: Props) {
 	return (
-		<section
-			style={{
-				display: 'flex',
-				gap: '1rem',
-				justifyContent: 'center',
-				alignItems: 'center',
-				backgroundColor: 'var(--secondary-color)',
-				borderRadius: '5px',
-
-				padding: '2rem',
-				margin: '3rem auto',
-			}}
+		<form
+			className="flex flex-col sm:flex-row gap-4 justify-center items-center rounded-md p-4 mx-auto my-0"
+			onSubmit={handleSubmit}
 		>
-			<form
-				style={{
-					display: 'flex',
-					gap: '1rem',
-					justifyContent: 'center',
-					alignItems: 'center',
-					backgroundColor: 'var(--terciary-color)',
-					borderRadius: '5px',
-					padding: '1rem',
-				}}
-				onSubmit={handleSubmit}
-			>
-				<label style={{ fontSize: '1.2rem' }} htmlFor={inputName}>
-					Your location
-				</label>
-				<input
-					required
-					type="text"
-					name={inputName}
-					id={inputName}
-					placeholder="London"
-				/>
+			<label className="text-lg" htmlFor={inputName}>
+				Your location
+			</label>
+			<input
+				required
+				type="text"
+				name={inputName}
+				id={inputName}
+				placeholder="London"
+			/>
 
-				<button type="submit">Submit</button>
-			</form>
-
-			<h3 id="location-form-divider">or</h3>
-
-			<div
-				style={{
-					backgroundColor: 'var(--terciary-color)',
-					borderRadius: '5px',
-					padding: '1rem',
-				}}
-			>
-				<button type="button" onClick={handleDetectUserLocation}>
-					Detect my location 🔎
-				</button>
-			</div>
-		</section>
+			<button type="submit">Submit</button>
+		</form>
 	);
 }
