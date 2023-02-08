87351f08e1873911ee7afc782f70614ff59823f4
genaroibc <bonavitaismael@gmail.com>
2023-02-08 16:25:23 -0300

    [FEAT]: pass data fetching responsibility to WeatherVideoPlayer

--- src/components/WeatherVideoPlayer.tsx
+++ src/components/WeatherVideoPlayer.tsx
@@ -6,67 +6,57 @@ import {
 	FPS,
 } from '@/remotion/constants';
 import { Player } from '@remotion/player';
+import { useEffect, useState } from 'react';
+import { Weather } from '../types/api-reponse';
+import { getWeatherData } from '../services/getWeatherData';
+import { Coordinates } from '@/types/globals';
 
-export function WeatherVideoPlayer() {
+type Props = {
+	locationCoords: Coordinates;
+};
+
+export function WeatherVideoPlayer({ locationCoords }: Props) {
+	const [weatherData, setWeatherData] = useState<Weather | null>(null);
+
+	useEffect(() => {
+		setWeatherData(null);
+
+		const handleGetWeatherData = async (coordinates: Coordinates) => {
+			const persistedWeatherData = JSON.parse(
+				localStorage.getItem('weatherData') ?? 'null'
+			);
+
+			if (!persistedWeatherData) {
+				const weatherData = await getWeatherData(coordinates);
+				localStorage.setItem('weatherData', JSON.stringify(weatherData));
+				setWeatherData(weatherData);
+
+				return;
+			}
+
+			setWeatherData(persistedWeatherData);
+		};
+
+		handleGetWeatherData(locationCoords);
+	}, [locationCoords]);
+
 	return (
-		<div
-			style={{
-				textAlign: 'center',
-				backgroundColor: 'var(--terciary-color)',
-				maxWidth: 'max-content',
-				margin: 'auto',
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				padding: '1rem',
-				borderRadius: '5px',
-			}}
-		>
-			<Player
-				controls
-				component={MainComposition}
-				compositionHeight={COMPOSITION_HEIGHT}
-				compositionWidth={COMPOSITION_WIDTH}
-				durationInFrames={DURATION_IN_FRAMES}
-				fps={FPS}
-				inputProps={{
-					coord: { lon: -0.1257, lat: 51.5085 },
-					weather: [
-						{
-							id: 804,
-							main: 'Clouds',
-							description: 'overcast clouds',
-							icon: '04d',
-						},
-					],
-					base: 'stations',
-					main: {
-						temp: 4.12,
-						feels_like: 1.8,
-						temp_min: 2.86,
-						temp_max: 5.56,
-						pressure: 1037,
-						humidity: 80,
-					},
-					visibility: 10_000, // in metters. Max is 10_000 metters
-					wind: { speed: 2.57 /* metters/second */, deg: 60 }, // wind direction in degrees
-					clouds: {
-						all: 100 /* percentage of clouds. 100% is so cloudly (implement a message for this) */,
-					},
-					dt: 1674576100,
-					sys: {
-						type: 2,
-						id: 2075535,
-						country: 'GB',
-						sunrise: 1674546627,
-						sunset: 1674578053,
-					},
-					timezone: 0, // utc time zone
-					id: 2643743,
-					name: 'London',
-					cod: 200,
-				}}
-			/>
-		</div>
+		<>
+			{weatherData ? (
+				<div className="text-center bg-terciary-color max-w-max mx-auto flex justify-center items-center p-4 rounded-md">
+					<Player
+						controls
+						component={MainComposition}
+						compositionHeight={COMPOSITION_HEIGHT}
+						compositionWidth={COMPOSITION_WIDTH}
+						durationInFrames={DURATION_IN_FRAMES}
+						fps={FPS}
+						inputProps={weatherData}
+					/>
+				</div>
+			) : (
+				<p>Fetching data...</p>
+			)}
+		</>
 	);
 }
--- src/remotion/Root.tsx
+++ src/remotion/Root.tsx
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
-import { Composition, continueRender, delayRender } from 'remotion';
+import { Weather } from '@/types/api-reponse';
+import { Composition } from 'remotion';
 import { MainComposition } from './compositions/MainComposition';
 import {
 	COMPOSITION_HEIGHT,
@@ -8,47 +8,21 @@ import {
 	FPS,
 } from './constants';
 import './styles/index.css';
-import { Weather } from '../types/api-reponse';
-import { getWeatherData } from '../services/getWeatherData';
 
-export const RemotionRoot: React.FC = () => {
-	const [weatherData, setWeatherData] = useState<Weather | null>(null);
-	const [handle] = useState(() => delayRender());
-
-	const handleGetWeatherData = useCallback(async () => {
-		const persistedWeatherData = JSON.parse(
-			localStorage.getItem('weatherData') ?? 'null'
-		);
-
-		if (!persistedWeatherData) {
-			const weatherData = await getWeatherData({ location: 'london' });
-			localStorage.setItem('weatherData', JSON.stringify(weatherData));
-			setWeatherData(weatherData);
-
-			return continueRender(handle);
-		}
-
-		setWeatherData(persistedWeatherData);
-		continueRender(handle);
-	}, [handle]);
-
-	useEffect(() => {
-		handleGetWeatherData();
-	}, [handleGetWeatherData]);
-
-	return (
-		<>
-			{weatherData && (
-				<Composition
-					id="MyComposition"
-					component={MainComposition}
-					defaultProps={weatherData}
-					fps={FPS}
-					durationInFrames={DURATION_IN_FRAMES}
-					width={COMPOSITION_WIDTH}
-					height={COMPOSITION_HEIGHT}
-				/>
-			)}
-		</>
-	);
+type Props = {
+	weatherData: Weather;
 };
+
+export function RemotionRoot({ weatherData }: Props) {
+	return (
+		<Composition
+			id="MyComposition"
+			component={MainComposition}
+			defaultProps={weatherData}
+			fps={FPS}
+			durationInFrames={DURATION_IN_FRAMES}
+			width={COMPOSITION_WIDTH}
+			height={COMPOSITION_HEIGHT}
+		/>
+	);
+}
