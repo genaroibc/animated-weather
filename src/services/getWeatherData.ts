d99b265d656746766395a592c3c553e61ba15678
genaroibc <bonavitaismael@gmail.com>
2023-02-08 16:30:59 -0300

    [DEV]: fetch data using coords instead of city name

--- src/services/getWeatherData.ts
+++ src/services/getWeatherData.ts
@@ -1,18 +1,24 @@
+import { Coordinates } from '@/types/globals';
 import { Weather } from '../types/api-reponse';
 
-const { NEXT_PUBLIC_API_URL: API_URL, API_KEY } = process.env;
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 const ERROR_MESSAGE = 'env variable is not defined';
 
-if (!API_URL) throw new Error(`"API_URL" ${ERROR_MESSAGE}`);
-if (!API_KEY)
-	throw new Error(`"API_KEY" ${ERROR_MESSAGE} - ` + process.env.API_KEY);
+if (!API_URL) {
+	throw new Error(`"API_URL" ${ERROR_MESSAGE}`);
+}
 
-type Params = {
-	location: string;
-};
+if (!API_KEY) {
+	throw new Error(`"API_KEY" ${ERROR_MESSAGE}`);
+}
 
-export function getWeatherData({ location }: Params): Promise<Weather> {
-	const URL = `${API_URL}?appid=${API_KEY}&q=${location}&units=metric`;
+export function getWeatherData({
+	latitude,
+	longitude,
+}: Coordinates): Promise<Weather> {
+	const URL = `${API_URL}?appid=${API_KEY}&lat=${latitude}&lon=${longitude}&units=metric`;
 
 	return fetch(URL).then((res) => res.json());
 }
