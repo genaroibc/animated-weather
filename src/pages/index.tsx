8182c28a82852ff36bb4cc4bf41c7129a7956e11
genaroibc <bonavitaismael@gmail.com>
2023-02-08 16:29:01 -0300

    [REFACTOR]: implement hook to get user location and style with tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,66 +1,57 @@
-import { useState } from 'react';
 import { WeatherVideoPlayer } from '@/components/WeatherVideoPlayer';
 import { LocationForm } from '@/components/LocationForm';
 import { Hero } from '@/components/Hero';
-
 const LOCATION_INPUT_NAME = 'user-location';
-
-type LocationState = string | { longitude: number; latitude: number };
+import { useUserLocation } from '@/hooks/useUserLocation';
+import { getCoordsByCityName } from '@/services/getCoordsByCityName';
+import { useState } from 'react';
+import { Coordinates } from '@/types/globals';
 
 export default function Home() {
-	const [userLocation, setUserLocation] = useState<LocationState>();
+	const { handleLocationRequest, userCoordinates, locationError } =
+		useUserLocation({ isInmediate: false });
+	const [resolvedCoords, setResolvedCoords] = useState<Coordinates | null>(
+		null
+	);
 
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault();
 
 		const form = e.target as HTMLFormElement;
 
-		const location = form[LOCATION_INPUT_NAME]?.value;
-		const userLocation = location?.trim();
+		const locationInput = form[LOCATION_INPUT_NAME]?.value;
+		const userLocation = locationInput?.trim();
 
 		if (userLocation) {
-			setUserLocation(userLocation);
+			getCoordsByCityName(userLocation).then(setResolvedCoords);
 		}
 	};
-
-	const handleDetectUserLocation = () => {
-		const onSuccess: PositionCallback = (res) => {
-			console.log({ res });
-			const { latitude, longitude } = res.coords;
-
-			setUserLocation({ latitude, longitude });
-		};
-
-		const onError: PositionErrorCallback = () => {
-			setUserLocation('There was an error accessing to your location.');
-			// -> TODO: setError()
-		};
-
-		navigator.geolocation.getCurrentPosition(onSuccess, onError);
-	};
-
+	console.log({ userCoordinates, locationError });
 	return (
-		<main
-			style={{
-				margin: '1rem auto',
-				display: 'flex',
-				justifyContent: 'center',
-				flexDirection: 'column',
-				gap: '1rem',
-			}}
-		>
+		<main className="mx-auto my-4 flex justify-center flex-col gap-4 text-center">
 			<Hero />
 
 			<LocationForm
-				handleDetectUserLocation={handleDetectUserLocation}
 				handleSubmit={handleSubmit}
 				inputName={LOCATION_INPUT_NAME}
 			/>
 
-			{userLocation ? (
-				<WeatherVideoPlayer />
+			<p>or</p>
+
+			<button className="mx-auto" type="button" onClick={handleLocationRequest}>
+				Detect my location 🔎
+			</button>
+
+			{resolvedCoords ? (
+				<WeatherVideoPlayer locationCoords={resolvedCoords} />
 			) : (
-				<h3>Enter a location to render your video...</h3>
+				<h3>
+					{locationError?.message ? (
+						locationError.message
+					) : (
+						<p className="my-4">Give us a location to render your video...</p>
+					)}
+				</h3>
 			)}
 		</main>
 	);
