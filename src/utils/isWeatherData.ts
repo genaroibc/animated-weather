a0efcf36c3ab1bef6af0a5fb2db5eae3a2319577
genaroibc <bonavitaismael@gmail.com>
2023-02-10 17:34:30 -0300

    [FIX]: cast type to avoid warnings

--- src/utils/isWeatherData.ts
+++ src/utils/isWeatherData.ts
@@ -2,20 +2,24 @@ import { Weather } from '@/types/api-reponse';
 import { isEmptyValue } from './isEmptyValue';
 
 export function isWeatherData(data: unknown): data is Weather {
+	if (!(data instanceof Object)) return false;
+
+	const castedData = data as Record<string, unknown>;
+
 	return (
-		!isEmptyValue(data) &&
-		!isEmptyValue(data.coord) &&
-		!isEmptyValue(data.weather) &&
-		!isEmptyValue(data.base) &&
-		!isEmptyValue(data.main) &&
-		!isEmptyValue(data.visibility) &&
-		!isEmptyValue(data.wind) &&
-		!isEmptyValue(data.clouds) &&
-		!isEmptyValue(data.dt) &&
-		!isEmptyValue(data.sys) &&
-		!isEmptyValue(data.timezone) &&
-		!isEmptyValue(data.id) &&
-		!isEmptyValue(data.name) &&
-		!isEmptyValue(data.cod)
+		!isEmptyValue(castedData) &&
+		!isEmptyValue(castedData.coord) &&
+		!isEmptyValue(castedData.weather) &&
+		!isEmptyValue(castedData.base) &&
+		!isEmptyValue(castedData.main) &&
+		!isEmptyValue(castedData.visibility) &&
+		!isEmptyValue(castedData.wind) &&
+		!isEmptyValue(castedData.clouds) &&
+		!isEmptyValue(castedData.dt) &&
+		!isEmptyValue(castedData.sys) &&
+		!isEmptyValue(castedData.timezone) &&
+		!isEmptyValue(castedData.id) &&
+		!isEmptyValue(castedData.name) &&
+		!isEmptyValue(castedData.cod)
 	);
 }
