c3674840341b1b05e9b8072c5cc0afb0fc462398
genaroibc <bonavitaismael@gmail.com>
2023-02-10 14:55:03 -0300

    [DEV]: add KnownError type

--- src/types/globals.ts
+++ src/types/globals.ts
@@ -3,3 +3,8 @@ export type TemperatureUnit = 'degrees' | 'farenheit';
 export type HideDirection = 'left' | 'right' | 'top' | 'bottom';
 
 export type Coordinates = { latitude: number; longitude: number };
+
+export type KnownError = {
+	ok: false;
+	message: string;
+};
