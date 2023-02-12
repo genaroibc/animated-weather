04048a0674a0a8c98dbad7789f9d14bb884a18f8
genaroibc <bonavitaismael@gmail.com>
2023-02-12 18:41:47 -0300

    [FEAT]: download rendered video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,38 @@
 import { Hero } from '@/components/Hero';
 import { VideoSection } from '@/components/VideoSection';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+	const [videoConfig, setVideoConfig] = useState<{ src: string } | null>();
+
+	useEffect(() => {
+		fetch('/api/render')
+			.then((response) => {
+				return response.blob();
+			})
+			.then((blob) => {
+				const videoUrl = URL.createObjectURL(blob);
+
+				setVideoConfig({ src: videoUrl });
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, []);
+
 	return (
 		<main className="mx-auto my-4 flex justify-center flex-col gap-4 text-center">
 			<Hero />
-
 			<VideoSection />
+
+			{videoConfig && (
+				<>
+					<video autoPlay controls src={videoConfig.src} />
+					<a download href={videoConfig.src}>
+						Download video
+					</a>
+				</>
+			)}
 		</main>
 	);
 }
