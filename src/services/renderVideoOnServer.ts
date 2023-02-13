b82a99a6117f4aa22b13dee7237c4046aae5e369
genaroibc <bonavitaismael@gmail.com>
2023-02-13 14:15:16 -0300

    [DEV]: add service to render video on the server

---
+++ src/services/renderVideoOnServer.ts
@@ -0,0 +1,26 @@
+import { Weather } from '@/types/api-reponse';
+import { KnownError } from '@/types/globals';
+
+export async function renderVideoOnServer(
+	weatherData: Weather
+): Promise<{ ok: true; data: string } | KnownError> {
+	try {
+		const response = await fetch('/api/render', {
+			method: 'POST',
+			body: JSON.stringify(weatherData),
+		});
+
+		const videoBlob = await response.blob();
+		const videoUrl = URL.createObjectURL(videoBlob);
+
+		// to do: validate videoUrl
+		return { ok: true, data: videoUrl };
+	} catch (error) {
+		console.error(error);
+		return {
+			ok: false,
+			message:
+				'There was an error rendering your video. Please try again later.',
+		};
+	}
+}
