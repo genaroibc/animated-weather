889ee3285469b25cdeb70253b3bc2d6c4a35111a
genaroibc <bonavitaismael@gmail.com>
2023-02-13 12:51:55 -0300

    [FEAT]: add animation for entering compositions

--- src/remotion/components/CompositionLayout.tsx
+++ src/remotion/components/CompositionLayout.tsx
@@ -1,5 +1,12 @@
-import { interpolate, spring, useCurrentFrame, useVideoConfig } from 'remotion';
-import { DURATION_IN_FRAMES } from '../constants';
+import {
+	AbsoluteFill,
+	interpolate,
+	spring,
+	useCurrentFrame,
+	useVideoConfig,
+} from 'remotion';
+import { TRANSITION_DURATION } from '../constants';
+import { EnterInView } from './EnterInView';
 
 type Props = {
 	children: React.ReactNode;
@@ -7,46 +14,22 @@ type Props = {
 
 export function CompositionLayout({ children }: Props) {
 	const frame = useCurrentFrame();
-	const { height, fps } = useVideoConfig();
+	const { fps, durationInFrames, width } = useVideoConfig();
 
-	const entrance = spring({
+	const spr = spring({
 		fps,
-		frame,
-		config: {
-			damping: 300,
-		},
-		durationInFrames: 10,
+		frame: frame - (durationInFrames - TRANSITION_DURATION),
+		config: { damping: 200 },
+		durationInFrames: TRANSITION_DURATION,
 	});
 
-	const entranceOffset = interpolate(entrance, [0, 1], [height, 0]);
-
-	const wave1 = Math.cos(frame / 15) * 10 + entranceOffset;
-
-	const PROGRESS = (frame / DURATION_IN_FRAMES) * 9;
-
-	const opacity =
-		interpolate(frame, [20, 40], [0, 1], {
-			extrapolateLeft: 'clamp',
-			extrapolateRight: 'clamp',
-		}) / 0.8;
-
 	return (
-		<div
-			className="flex flex-col justify-center items-center w-full text-2xl gap-12 m-auto font-bold transition-transform duration-1000 ease-out"
+		<AbsoluteFill
 			style={{
-				transition: 'opacity .3 ease',
-				opacity: PROGRESS > 1.5 ? Math.asinh(PROGRESS) : opacity,
-				transform:
-					`translateY(${wave1}px)
-        ` +
-					(PROGRESS > 1.6
-						? `translateX(-${200}%)`
-						: PROGRESS > 1.5
-						? `translateX(${10}%)`
-						: ''),
+				transform: `translateX(${interpolate(spr, [0, 1], [0, -width])}px)`,
 			}}
 		>
-			{children}
-		</div>
+			<EnterInView>{children}</EnterInView>
+		</AbsoluteFill>
 	);
 }
