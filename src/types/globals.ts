e831e2cebf2c14b3340427fa6609dff20acf8935
genaroibc <bonavitaismael@gmail.com>
2023-02-16 18:16:06 -0300

    [DEV]: remove unused type

--- src/types/globals.ts
+++ src/types/globals.ts
@@ -1,7 +1,5 @@
 export type TemperatureUnit = 'degrees' | 'farenheit';
 
-export type HideDirection = 'left' | 'right' | 'top' | 'bottom';
-
 export type Coordinates = { latitude: number; longitude: number };
 
 export interface KnownError {
