d64a209a20e0a3ef2cc11ea399cfc0d9b8ae20ff
genaroibc <bonavitaismael@gmail.com>
2023-02-16 11:37:06 -0300

    [FEAT]: style Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,9 @@
 export function Hero() {
 	return (
 		<header>
-			<h1 className="text-center my-12 mx-auto text-5xl">Animated Weather</h1>
+			<h1 className="text-center my-12 mx-auto text-5xl font-bold">
+				Animated Weather
+			</h1>
 			<h2 className="text-3xl text-center">
 				Create an animated video of the weather in your zone
 			</h2>
