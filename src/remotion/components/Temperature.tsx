8ad7823b5a85a6b297baa6b18e0199a4aa014e45
genaroibc <bonavitaismael@gmail.com>
2023-02-17 16:53:21 -0300

    [FEAT]: show different image depending on input temperature

--- src/remotion/components/Temperature.tsx
+++ src/remotion/components/Temperature.tsx
@@ -1,13 +1,27 @@
 import { TEMPERATURE_UNIT } from '../constants';
 import { WeatherMain } from '../../types/api-reponse';
 import { CompositionLayout } from './shared/CompositionLayout';
+import { useCurrentFrame } from 'remotion';
+import { ColdText } from './shared/ColdText';
 
 type Props = WeatherMain;
 
 export function Temperature({ feels_like, temp, temp_max, temp_min }: Props) {
+	const frame = useCurrentFrame();
+
 	return (
 		<CompositionLayout from="bottom" to="right" title="Temperature">
-			<span>The temperature is {temp} </span>
+			<span
+				style={{
+					clipPath: `circle(${frame * 3.5}%)`,
+				}}
+			>
+				The temperature is{' '}
+				<span className="text-4xl">
+					<ColdText degrees={temp} content={String(temp)} />
+				</span>{' '}
+				<span>{TEMPERATURE_UNIT}</span>
+			</span>
 
 			<span>It feels like: {`${feels_like} ${TEMPERATURE_UNIT}`}</span>
 
