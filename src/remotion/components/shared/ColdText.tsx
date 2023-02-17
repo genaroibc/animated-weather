4a1e6d35cfe90c58e118482095160a370ed0f79b
genaroibc <bonavitaismael@gmail.com>
2023-02-17 16:51:48 -0300

    [DEV]: add ColdText component

---
+++ src/remotion/components/shared/ColdText.tsx
@@ -0,0 +1,20 @@
+type Props = {
+	content: string;
+	degrees: number;
+};
+
+export function ColdText({ content, degrees }: Props) {
+	return (
+		<span
+			style={{
+				background: `url(/${degrees > 20 ? 'fire' : 'ice'}.jpg)`,
+				WebkitBackgroundClip: 'text',
+				backgroundClip: 'text',
+			}}
+			className="cold"
+			data-heading={content}
+		>
+			{content}
+		</span>
+	);
+}
